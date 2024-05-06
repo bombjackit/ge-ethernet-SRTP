faa42d3174316cf5e8cc5360fbdaa93d1692be57
Dilpreet Chana <dschana6@gmail.com>
2024-05-06 09:37:26 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ge_ethernet_srtp',
+    version='0.1',
+    packages=find_packages('lib'),
+    package_dir={'': 'lib'},
+    author='TheMadHatt3r',
+    description='A description of your library',
+    long_description='A longer description of your library',
+    long_description_content_type='text/markdown',
+    url='https://github.com/DSchana/ge-ethernet-SRTP',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='Emerson GE PLC Libarary',
+    project_urls={
+        'Source': 'https://github.com/DSchana/ge-ethernet-SRTP',
+        'Bug Reports': 'https://github.com/TheMadHatt3r/ge-ethernet-SRTP/issues',
+    },
+)
